1cab089deb67516eef1d4b4cf51dd7bf4a0cbbc1
abror9603 <avazovabror1996@gmail.com>
2025-03-01 15:21:27 +0500

    ProductController va bir qancha resourcelar qilindi

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-
+use Illuminate\Routing\Controllers\Middleware;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 class ProductController extends Controller
 {
+    public static function middleware(): array
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show']),
+        ];
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return ProductResource::collection(Product::cursorPaginate(20));
     }
 
     /**
@@ -29,7 +39,9 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        $product = Product::create($request->toArray());
+
+        return $this->success('product created', new ProductResource($product));
     }
 
     /**
@@ -37,7 +49,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
@@ -61,6 +73,13 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        Gate::authorize('product:delete');
+
+        Storage::delete($product->photos()->pluck('path')->toArray());
+
+        $product->photos()->delete();
+        $product->delete();
+
+        return $this->success('product deleted');
     }
 }
