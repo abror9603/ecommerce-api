aa8fba2c219351d2c7af3b40f5284de57f57d401
abror9603 <avazovabror1996@gmail.com>
2025-03-07 12:06:18 +0500

    Order, Status, PaymentType, DeliveryMethod modellari qoshildi

---
+++ app/Models/Order.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'comment',
+        'delivery_method_id',
+        'payment_type_id',
+        'sum',
+        'status_id',
+        'products',
+        'address',
+    ];
+
+    protected $casts = [
+        'products' => 'array',
+        'address' => 'array',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function deliveryMethod(): BelongsTo
+    {
+       return $this->belongsTo(DeliveryMethod::class); 
+    }
+
+    public function paymentType(): BelongsTo
+    {
+        return $this->belongsTo(PaymentType::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+}
